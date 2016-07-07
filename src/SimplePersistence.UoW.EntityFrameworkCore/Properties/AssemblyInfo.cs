7a5498d77ee5150470520ce322b87e134fedbb8c
João Simões <nogravity00@outlook.com>
2016-07-07 23:14:11 +0100

    Added main project

---
+++ src/SimplePersistence.UoW.EntityFrameworkCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿#region License
+// The MIT License (MIT)
+// 
+// Copyright (c) 2016 SimplePersistence
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#endregion
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimplePersistence.UoW.EntityFrameworkCore")]
+[assembly: AssemblyDescription("SimplePersistence.UoW.EntityFrameworkCore offers implementations to the SimplePersistence.UoW using the Entity Framework Core as the ORM.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Net.JoaoSimoes")]
+[assembly: AssemblyProduct("SimplePersistence")]
+[assembly: AssemblyCopyright("Copyright © 2016 SimplePersistence")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("aaaa215f-2f8e-48e9-9a8a-7bb80501b666")]
+
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("3.3.0")]
+[assembly: AssemblyInformationalVersion("3.3.0-alpha01")]
